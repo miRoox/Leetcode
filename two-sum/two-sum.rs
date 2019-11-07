4ee2260f2360a83b41eab59b22f83bd75215bf1f
miRoox <miroox@outlook.com>
2019-11-07 21:36:53 +0800

    two-sum

---
+++ two-sum/two-sum.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut i1=0;
+        while i1<nums.len() {
+            let mut i2=i1+1;
+            while i2<nums.len() {
+                if nums[i1]+nums[i2]==target {
+                    return vec![i1 as i32,i2 as i32]
+                }
+                i2+=1
+            }
+            i1+=1
+        }
+        vec![0,1]
+    }
+}
