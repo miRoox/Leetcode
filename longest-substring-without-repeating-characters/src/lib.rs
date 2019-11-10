7337aa55180f1d185c9cc75640b74b665440aa38
miRoox <miroox@outlook.com>
2019-11-10 15:41:24 +0800

    refactoring
    cargo workspace
    add basic test cases

--- longest-substring-without-repeating-characters/src/lib.rs
+++ longest-substring-without-repeating-characters/src/lib.rs
@@ -1,3 +1,6 @@
+pub struct Solution { }
+
+//---
 use std::collections::HashMap;
 use std::cmp::max;
 
@@ -16,5 +19,29 @@ impl Solution {
             last.insert(v,i);
         }
         max(s.len()-start,longest) as i32
+    }
+}
+//---
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let instr = String::from("abcabcbb");
+        assert_eq!(Solution::length_of_longest_substring(instr), 3);
+    }
+
+    #[test]
+    fn single() {
+        let instr = String::from("bbbbb");
+        assert_eq!(Solution::length_of_longest_substring(instr), 1);
+    }
+
+    #[test]
+    fn subseq() {
+        let instr = String::from("pwwkew");
+        assert_eq!(Solution::length_of_longest_substring(instr), 3);
     }
 }
--- two-sum/src/lib.rs
+++ two-sum/src/lib.rs
@@ -1,3 +1,6 @@
+pub struct Solution { }
+
+
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut i1=0;
@@ -12,5 +15,21 @@ impl Solution {
             i1+=1
         }
         unreachable!()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(Solution::two_sum(vec![2,7,11,15], 9), vec![0,1]);
+    }
+
+    #[test]
+    fn last_two() {
+        assert_eq!(Solution::two_sum(vec![3,2,4], 6), vec![1,2]);
     }
 }
