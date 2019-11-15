6640959e1b50d57c59c15654d1d39ba1b49ee73d
miRoox <miroox@outlook.com>
2019-11-15 19:04:18 +0800

    fix overflow exception for reverse-integer

--- reverse-integer/src/lib.rs
+++ reverse-integer/src/lib.rs
@@ -4,10 +4,17 @@ pub struct Solution { }
 impl Solution {
     pub fn reverse(x: i32) -> i32 {
         let mut x = x;
-        let mut ans = 0;
+        let mut ans = 0i32;
         while x!=0 {
-            let pop = x%10;
-            ans = 10*ans + pop;
+            let (try_mul, overflowed) = ans.overflowing_mul(10);
+            if overflowed {
+                return 0;
+            }
+            let (try_add, overflowed) = try_mul.overflowing_add(x%10);
+            if overflowed {
+                return 0;
+            }
+            ans = try_add; // ans = 10*ans+x%10
             x /= 10;
         }
         ans
@@ -32,5 +39,15 @@ mod tests {
     #[test]
     fn ends_zero() {
         assert_eq!(Solution::reverse(120), 21);
+    }
+
+    #[test]
+    fn overflow() {
+        assert_eq!(Solution::reverse(1534236469), 0);
+    }
+
+    #[test]
+    fn underflow() {
+        assert_eq!(Solution::reverse(-1534236469), 0);
     }
 }
