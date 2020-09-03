c389060f29080561fd4b181c2d7d95ee08a574b3
miroox <miroox@outlook.com>
2020-09-03 13:16:15 +0800

    cpp solution for 'two sum'

--- cpp/two-sum/main.cpp
+++ cpp/two-sum/main.cpp
@@ -7,15 +7,25 @@ using std::vector;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        return {0, 1};
+        const auto size = nums.size();
+        for (int i = 0; i < size; ++i)
+        {
+            for (int j = i+1; j < size; ++j)
+            {
+                if (nums[i] + nums[j] == target)
+                    return {i, j};
+            }
+        }
+        return {};
     }
 };
 
 TEST_CASE("two sum") {
     Solution sol;
     SUBCASE("basic") {
-        vector<int> nums {2, 7, 11, 15};
-        vector<int> pair {0, 1};
-        CHECK(sol.twoSum(nums, 9) == pair);
+        CHECK(sol.twoSum(vector<int>{2, 7, 11, 15}, 9) == vector<int>{0, 1});
+    }
+    SUBCASE("last-two") {
+        CHECK(sol.twoSum(vector<int>{3, 2, 4}, 6) == vector<int>{1, 2});
     }
 }
