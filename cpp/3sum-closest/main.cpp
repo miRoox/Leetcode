b46dba1ca733a72672ed44fbbf9a382d474c4aef
miroox <miroox@outlook.com>
2020-09-04 20:20:55 +0800

    cpp solution for '3sum closest'

---
+++ cpp/3sum-closest/main.cpp
@@ -0,0 +1,46 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include <vector>
+
+using std::vector;
+
+#include <algorithm>
+#include <limits>
+#include <cmath>
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        std::sort(nums.begin(), nums.end());
+        int result = 0;
+        int min_diff = std::numeric_limits<int>::max();
+        int last = static_cast<int>(nums.size()-1);
+        for (int i=0; i<last; ++i) {
+            int j = i+1;
+            int k = last;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < target) {
+                    ++j;
+                } else if (sum > target) {
+                    --k;
+                } else {
+                    return target;
+                }
+                int diff = std::abs(sum-target);
+                if (diff < min_diff) {
+                    min_diff = diff;
+                    result = sum;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+TEST_CASE("3sum closest") {
+    Solution sol;
+    SUBCASE("basic") {
+        vector<int> nums{-1,2,1,-4};
+        CHECK(sol.threeSumClosest(nums, 1) == 2);
+    }
+}
