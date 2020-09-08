d259f5834121dfa9dc36873585eb4709867dae13
miroox <miroox@outlook.com>
2020-09-08 16:33:00 +0800

    optimize cpp solution for 'jump game'

--- cpp/jump-game/main.cpp
+++ cpp/jump-game/main.cpp
@@ -4,45 +4,50 @@
 
 using std::vector;
 
+#include <utility>
+using std::size_t;
 class Solution {
-    vector<bool> cached;
-    vector<bool> cache;
-
-    inline bool canJumpFrom(const vector<int>& nums, size_t start) {
-        if (start+1 >= nums.size()) {
-            return true;
-        }
-        if (cached[start]) {
-            return cache[start];
-        }
-        const int max = nums[start];
-        if (max==0){
-            cached[start] = true;
-            return cache[start] = false;
-        }
-        bool found = false;
-        for(int i=max; i>0 ; --i) {
-            found |= canJumpFrom(nums, start + i);
-            if (found)
+    inline size_t rfind_zero(const vector<int>& nums, size_t sentinel) {
+        size_t i = sentinel;
+        while (i--) {
+            if (nums[i] == 0)
                 break;
         }
-        cached[start] = true;
-        return cache[start] = found;
+        return i;
+    }
+    inline bool canJumpSpan(const vector<int>& nums, size_t sentinel) {
+        if (sentinel == 0)
+            return true;
+        const size_t iz = rfind_zero(nums, sentinel);
+        if (iz == static_cast<size_t>(-1)) 
+            return true;
+        for(size_t i = iz-1; i != static_cast<size_t>(-1); --i) {
+            if (i+nums[i] > iz)
+                return canJumpSpan(nums, i);
+        }
+        return false;
     }
 public:
     bool canJump(const vector<int>& nums) {
-        cache = vector<bool>(nums.size());
-        cached = vector<bool>(nums.size(), false);
-        return canJumpFrom(nums, 0);
+        return canJumpSpan(nums, nums.size()-1);
     }
 };
 
 TEST_CASE("jump game") {
     Solution sol;
-    SUBCASE("reachable") {
+    SUBCASE("without zero") {
         CHECK(sol.canJump({2,3,1,1,4}));
     }
-    SUBCASE("unreachable") {
+    SUBCASE("unreachable with zero") {
         CHECK(!sol.canJump({3,2,1,0,4}));
+    }
+    SUBCASE("reachable with zero") {
+        CHECK(sol.canJump({3,3,1,0,4}));
+    }
+    SUBCASE("reachable with double zeros") {
+        CHECK(sol.canJump({3,0,2,0,1,4}));
+    }
+    SUBCASE("unreachable with double zeros") {
+        CHECK(!sol.canJump({1,0,2,0,1,4}));
     }
 }
