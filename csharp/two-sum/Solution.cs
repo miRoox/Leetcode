db6e1241ef24e6ef64aaaabf3ac2d5a1a4ab3bf3
miroox <miroox@outlook.com>
2020-09-17 13:33:21 +0800

    first csharp solution:
    two sum

---
+++ csharp/two-sum/Solution.cs
@@ -0,0 +1,13 @@
+namespace two_sum {
+    public class Solution {
+        public int[] TwoSum(int[] nums, int target) {
+            for (int i = 0; i < nums.Length; i++) {
+                for (int j = 0; j < nums.Length; j++) {
+                    if (nums[i] + nums[j] == target)
+                        return new int[2] {i, j};
+                }
+            }
+            return new int[2];
+        }
+    }
+}
